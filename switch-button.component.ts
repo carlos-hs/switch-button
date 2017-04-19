8c805499881ec45866b09adb46ca99f107a55fb5
carlos-hs <kccarlos@hotmail.com>
2017-04-18 22:35:00 -0300

    Switch Button
    Added switch button.

---
+++ switch-button.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'switch-button',
+    templateUrl: './switch-button.component.html',
+    styleUrls: ['./switch-button.component.css']
+})
+
+export class SwitchButtonComponent {
+    @Input() value: boolean;
+    @Input() color: string;
+
+    onSwitchButton() {
+        this.value = !this.value;
+        console.log(this.value);
+    }
+}
---
+++ switch-button.module.ts
@@ -0,0 +1,9 @@
+﻿import { NgModule } from '@angular/core';
+import { SwitchButtonComponent } from './switch-button.component'; 
+
+@NgModule({
+    declarations: [SwitchButtonComponent],
+    exports: [SwitchButtonComponent]
+})
+
+export class SwitchButtonModule { }
